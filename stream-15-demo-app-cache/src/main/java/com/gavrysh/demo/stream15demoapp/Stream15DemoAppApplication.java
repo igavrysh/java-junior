a715d39489c67a8a871d926f2638a7da6adef6fa
“Gene <mickey3d19892@gmail.com>
2022-10-08 21:11:15 -0700

    refactor folder names

---
+++ stream-15-demo-app-cache/src/main/java/com/gavrysh/demo/stream15demoapp/Stream15DemoAppApplication.java
@@ -0,0 +1,13 @@
+package com.gavrysh.demo.stream15demoapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Stream15DemoAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Stream15DemoAppApplication.class, args);
+	}
+
+}
---
+++ stream-15-demo-app-cache/src/test/java/com/gavrysh/demo/stream15demoapp/Stream15DemoAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gavrysh.demo.stream15demoapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Stream15DemoAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
